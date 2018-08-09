020455c2e6c5f693c3406ad78f7b224e70239ae8
Joshua G Evans <jevan5@uwo.ca>
2018-08-09 17:57:37 -0400

    Re-structure Portfolio Model
    The Portfolio model was modified to keep track of which buys have
    been sold off already by the account.

--- backend/models/portfolio.js
+++ backend/models/portfolio.js
@@ -1,18 +1,22 @@
 var mongoose = require('mongoose');
 
-const tradeFormat = {
-    price: Number,
-    quantity: Number,
-    symbol: String,
-    timeStamp: Date
-};
-
 var PortfolioSchema = mongoose.Schema(
     {
         account: {type: mongoose.Schema.ObjectId, ref: 'Accounts'},
-        buys: [tradeFormat],
+        buys: [{
+            price: Number,      // price that the shares were each bought for
+            quantity: Number,   // how many shares were purchased
+            symbol: String,     // stock symbol purchased
+            timeStamp: Date,    // when the purchase occurred
+            remaining: Number   // how many of these shares have not yet been sold
+        }],
         name: String,
-        sells: [tradeFormat]
+        sells: [{
+            price: Number,      // price that the shares were sold for
+            quantity: Number,   // how many shares were sold
+            symbol: String,     // stock symbol sold
+            timeStamp: Date     // when the sale occurred
+        }]
     }
 );
 
